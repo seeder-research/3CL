c5bb3fe4597530c6eea2f74ee262d7515bfa6e94
Xuanyao Fong <xuanyao.fong@gmail.com>
2016-05-21 01:58:40 +0800

    Update initialization function and added release function

--- opencl/init.go
+++ opencl/init.go
@@ -18,14 +18,20 @@ var (
 	PlatformInfo	string     		// Human-readable OpenCL platform description
 	GPUInfo     	string     		// Human-readable GPU description
 	Synchronous 	bool       		// for debug: synchronize stream0 at every kernel launch
-	ClCtx       	cl.Context 		// global OpenCL context
-	ClProgram   	cl.Program		// handle to program in the global OpenCL context
+	ClPlatforms     []*cl.Platform          // list of platforms available
+	ClPlatform      *cl.Platform            // platform the global OpenCL context is attached to
+	ClDevices       []*cl.Device            // list of devices global OpenCL context may be associated with
+	ClDevice        *cl.Device              // device associated with global OpenCL context
+	ClCtx       	*cl.Context 		// global OpenCL context
+	ClCmdQueue      *cl.CommandQueue        // command queue attached to global OpenCL context
+	ClProgram   	*cl.Program		// handle to program in the global OpenCL context
 	KernList    	map[string]*cl.Kernel	// Store pointers to all compiled kernels
+	initialized     = false                 // Initial state defaults to false
 )
 
 // Locks to an OS thread and initializes CUDA for that thread.
 func Init(gpu, platformId int) {
-	if ClCtx.ContextQuery() != nil {
+	if (initialized) {
 		fmt.Printf("Already initialized \n")
 		return // needed for tests
 	}
@@ -44,7 +50,9 @@ func Init(gpu, platformId int) {
 	PlatformProfile := platform.Profile()
 	PlatformVersion := platform.Version()
 	PlatformInfo = fmt.Sprint("//   Name: ", PlatformName, "\n//   Vendor: ", PlatformVendor, "\n//   Profile: ", PlatformProfile, "\n//   Version: ", PlatformVersion,"\n")
-
+	ClPlatforms = platforms
+	ClPlatform = platform
+	
 	devices, err := platform.GetDevices(cl.DeviceTypeGPU)
 	if err != nil {
 		fmt.Printf("Failed to get devices: %+v \n", err)
@@ -72,27 +80,42 @@ func Init(gpu, platformId int) {
 	Version = devices[deviceIndex].OpenCLCVersion()
 	GPUInfo = fmt.Sprint("OpenCL C Version ", Version, "\n// GPU: ", DevName, "(", (TotalMem)/(1024*1024), "MB) \n")
 	device := devices[deviceIndex]
-	ClCtx, err := cl.CreateContext([]*cl.Device{device})
+	ClDevices = devices
+	ClDevice = device
+	context, err := cl.CreateContext([]*cl.Device{device})
 	if err != nil {
 		fmt.Printf("CreateContext failed: %+v \n", err)
 	}
-	ClProgram, err := ClCtx.CreateProgramWithSource([]string{kernels.GenMergedKernelSource()})
+	queue, err := context.CreateCommandQueue(device, 0)
+	if err != nil {
+		fmt.Printf("CreateCommandQueue failed: %+v \n", err)
+	}
+	program, err := context.CreateProgramWithSource([]string{kernels.GenMergedKernelSource()})
 	if err != nil {
 		fmt.Printf("CreateProgramWithSource failed: %+v \n", err)
 	}
-	if err := ClProgram.BuildProgram(nil, "-cl-std=CL1.2 -cl-kernel-arg-info"); err != nil {
+	if err := program.BuildProgram(nil, "-cl-std=CL1.2 -cl-kernel-arg-info"); err != nil {
 		fmt.Printf("BuildProgram failed: %+v \n", err)
 	}
 
 	KernList = map[string]*cl.Kernel{}
 	for i0 := range kernels.KernelsList {
 		kernName := kernels.KernelsList[i0]
-		KernList[kernName], err = ClProgram.CreateKernel(kernName)
+		KernList[kernName], err = program.CreateKernel(kernName)
 		if err != nil {
 		       fmt.Printf("CreateKernel failed: %+v \n", err)
 		}
 	}
+	ClCtx = context
+	ClCmdQueue = queue
+	ClProgram = program
 
+}
+
+func ReleaseAndClean() {
+	ClCmdQueue.Release()
+	ClProgram.Release()
+	ClCtx.Release()
 }
 
 // Global stream used for everything
