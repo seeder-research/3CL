91812cbf4aa9e3a67f46e0bc9fe27394650cce55
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2016-07-26 23:54:33 -0400

    Fixed clean.bash

--- clean.bash
+++ clean.bash
@@ -3,5 +3,7 @@
 rm -frv $GOPATH/pkg/*
 rm -frv $GOPATH/bin/*
 rm -fv  opencl/*_wrapper.go
+rm -fv  opencl/kernels_src/opencl2go
+rm -fv  opencl/kernels_src/cl/ocl2go
 rm -fv  opencl/kernels_src/*_wrapper.go
 rm -fv  opencl/kernels/*_wrapper.go
