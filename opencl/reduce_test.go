6bfcdba5b1d80fcc424d7af29e646c55c2c53e46
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2016-08-01 02:01:43 -0400

    Added test for MaxDiff

--- opencl/reduce_test.go
+++ opencl/reduce_test.go
@@ -83,6 +83,45 @@ func TestReduceMaxAbs(t *testing.T) {
 	}
 }
 
+func TestReduceMaxDiff(t *testing.T) {
+	// Test on a 1-D array first
+        ah1 := make([]float32, 1000)
+        bh1 := make([]float32, 1000)
+        for i := range ah1 {
+                ah1[i] = float32(i)
+		bh1[i] = float32(i+i)
+        }
+        a1 := toGPU(ah1)
+        b1 := toGPU(bh1)
+	result := MaxDiff(a1, b1)
+	if (len(result) != 1) {
+		t.Error("unexpected result length:", len(result))
+	}
+	if result[0] != (bh1[999] - ah1[999]) {
+		t.Error("got:", result)
+	}
+	// Check on same 1-D array but change specific
+	// entry in b1 first
+	SetElem(b1, 0, 1, 10000)
+	result = MaxDiff(a1, b1)
+	if result[0] != 9999 {
+		t.Error("got:", result)
+	}
+
+	// Check on 3-D array but each element in list of 32
+	// has the same value
+	const N = 32
+	mesh := [3]int{1, 1, N}
+	a1 = NewSlice(3, mesh)
+	b1 = NewSlice(3, mesh)
+	Memset(a1, 1, 2, 3)
+	Memset(b1, 3, 6, 9)
+	result = MaxDiff(a1, b1)
+	if ((result[0] != 2) || (result[1] != 4) || (result[2] != 6)) {
+		t.Error("got:", result)
+	}
+}
+
 func TestReduceMaxVecDiff(t *testing.T) {
 	const N = 32
 	mesh := [3]int{1, 1, N}
