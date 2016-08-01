b37e8534bb80a0c44754e2fa64e521436a8538f2
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2016-08-01 05:53:21 -0400

    Fixed madd kernels in opencl package

--- opencl/madd_cuda.go
+++ opencl/madd_cuda.go
@@ -1,8 +1,11 @@
-package cuda
+package opencl
 
 import (
-	"github.com/mumax/3/data"
-	"github.com/mumax/3/util"
+	"fmt"
+
+	"github.com/mumax/3cl/opencl/cl"
+	"github.com/mumax/3cl/data"
+	"github.com/mumax/3cl/util"
 )
 
 // multiply: dst[i] = a[i] * b[i]
@@ -11,8 +14,21 @@ func Mul(dst, a, b *data.Slice) {
 	nComp := dst.NComp()
 	util.Assert(a.Len() == N && a.NComp() == nComp && b.Len() == N && b.NComp() == nComp)
 	cfg := make1DConf(N)
+	eventList := make([]*cl.Event, nComp)
 	for c := 0; c < nComp; c++ {
-		k_mul_async(dst.DevPtr(c), a.DevPtr(c), b.DevPtr(c), N, cfg)
+		eventList[c] = k_mul_async(dst.DevPtr(c), a.DevPtr(c), b.DevPtr(c), N, cfg,
+			    		   [](*cl.Event){dst.GetEvent(c), a.GetEvent(c), b.GetEvent(c)})
+		dst.SetEvent(c, eventList[c])
+		a.SetEvent(c, eventList[c])
+		b.SetEvent(c, eventList[c])
+	}
+	bar, err := ClCmdQueue.EnqueueBarrierWithWaitList(eventList)
+	if err != nil {
+		fmt.Printf("EnqueueBarrierWithWaitList failed in mul: %+v \n", err)
+	}
+	err = cl.WaitForEvents([]*cl.Event{bar})
+	if err != nil {
+		fmt.Printf("WaitForEvents failed in mul: %+v \n", err)
 	}
 }
 
@@ -23,9 +39,22 @@ func Madd2(dst, src1, src2 *data.Slice, factor1, factor2 float32) {
 	util.Assert(src1.Len() == N && src2.Len() == N)
 	util.Assert(src1.NComp() == nComp && src2.NComp() == nComp)
 	cfg := make1DConf(N)
+	eventList := make([]*cl.Event, nComp)
 	for c := 0; c < nComp; c++ {
-		k_madd2_async(dst.DevPtr(c), src1.DevPtr(c), factor1,
-			src2.DevPtr(c), factor2, N, cfg)
+		eventList[c] = k_madd2_async(dst.DevPtr(c), src1.DevPtr(c), factor1,
+			src2.DevPtr(c), factor2, N, cfg,
+			[](*cl.Event){dst.GetEvent(c), src1.GetEvent(c), src2.GetEvent(c)})
+		dst.SetEvent(c, eventList[c])
+		src1.SetEvent(c, eventList[c])
+		src2.SetEvent(c, eventList[c])
+	}
+	bar, err := ClCmdQueue.EnqueueBarrierWithWaitList(eventList)
+	if err != nil {
+		fmt.Printf("EnqueueBarrierWithWaitList failed in madd2: %+v \n", err)
+	}
+	err = cl.WaitForEvents([]*cl.Event{bar})
+	if err != nil {
+		fmt.Printf("WaitForEvents failed in madd2: %+v \n", err)
 	}
 }
 
@@ -36,8 +65,23 @@ func Madd3(dst, src1, src2, src3 *data.Slice, factor1, factor2, factor3 float32)
 	util.Assert(src1.Len() == N && src2.Len() == N && src3.Len() == N)
 	util.Assert(src1.NComp() == nComp && src2.NComp() == nComp && src3.NComp() == nComp)
 	cfg := make1DConf(N)
+	eventList := make([]*cl.Event, nComp)
 	for c := 0; c < nComp; c++ {
-		k_madd3_async(dst.DevPtr(c), src1.DevPtr(c), factor1,
-			src2.DevPtr(c), factor2, src3.DevPtr(c), factor3, N, cfg)
+		eventList[c] = k_madd3_async(dst.DevPtr(c), src1.DevPtr(c), factor1,
+			src2.DevPtr(c), factor2, src3.DevPtr(c), factor3, N, cfg,
+			[](*cl.Event){dst.GetEvent(c), src1.GetEvent(c),
+			src2.GetEvent(c), src3.GetEvent(c)})
+		dst.SetEvent(c, eventList[c])
+		src1.SetEvent(c, eventList[c])
+		src2.SetEvent(c, eventList[c])
+		src3.SetEvent(c, eventList[c])
+	}
+	bar, err := ClCmdQueue.EnqueueBarrierWithWaitList(eventList)
+	if err != nil {
+		fmt.Printf("EnqueueBarrierWithWaitList failed in madd3: %+v \n", err)
+	}
+	err = cl.WaitForEvents([]*cl.Event{bar})
+	if err != nil {
+		fmt.Printf("WaitForEvents failed in madd3: %+v \n", err)
 	}
 }
