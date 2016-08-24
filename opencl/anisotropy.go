a501ddd96c57b82a46b764f4c284313335b2df53
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2016-08-23 23:52:55 -0400

    Clean anisotropy, dmi and dmibulk

---
+++ opencl/anisotropy.go
@@ -0,0 +1,61 @@
+package opencl
+
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/mumax/3cl/opencl/cl"
+	"github.com/mumax/3cl/data"
+	"github.com/mumax/3cl/util"
+)
+
+// Adds cubic anisotropy field to Beff.
+func AddCubicAnisotropy(Beff, m *data.Slice, k1_red, k2_red, k3_red LUTPtr, c1, c2 LUTPtrs, regions *Bytes) {
+	util.Argument(Beff.Size() == m.Size())
+
+	N := Beff.Len()
+	cfg := make1DConf(N)
+
+	event := k_addcubicanisotropy_async(
+		Beff.DevPtr(X), Beff.DevPtr(Y), Beff.DevPtr(Z),
+		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
+		unsafe.Pointer(k1_red), unsafe.Pointer(k2_red), unsafe.Pointer(k3_red),
+		c1[X], c1[Y], c1[Z],
+		c2[X], c2[Y], c2[Z],
+		regions.Ptr, N, cfg, [](*cl.Event){Beff.GetEvent(X), 
+		Beff.GetEvent(Y), Beff.GetEvent(Z), m.GetEvent(X), m.GetEvent(Y), m.GetEvent(Z)})
+
+	Beff.SetEvent(X, event)
+	Beff.SetEvent(Y, event)
+	Beff.SetEvent(Z, event)
+	m.SetEvent(X, event)
+	m.SetEvent(Y, event)
+	m.SetEvent(Z, event)
+	err := cl.WaitForEvents([](*cl.Event){event})
+	if err != nil { fmt.Printf("WaitForEvents failed in addcubicanisotropy: %+v \n", err) }
+}
+
+// Add uniaxial magnetocrystalline anisotropy field to Beff.
+// see uniaxialanisotropy.cu
+func AddUniaxialAnisotropy(Beff, m *data.Slice, k1_red, k2_red LUTPtr, u LUTPtrs, regions *Bytes) {
+	util.Argument(Beff.Size() == m.Size())
+
+	N := Beff.Len()
+	cfg := make1DConf(N)
+
+	event := k_adduniaxialanisotropy_async(Beff.DevPtr(X), Beff.DevPtr(Y), Beff.DevPtr(Z),
+		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
+		unsafe.Pointer(k1_red), unsafe.Pointer(k2_red),
+		u[X], u[Y], u[Z],
+		regions.Ptr, N, cfg, [](*cl.Event){Beff.GetEvent(X),
+                Beff.GetEvent(Y), Beff.GetEvent(Z), m.GetEvent(X), m.GetEvent(Y), m.GetEvent(Z)})
+
+	Beff.SetEvent(X, event)
+	Beff.SetEvent(Y, event)
+	Beff.SetEvent(Z, event)
+	m.SetEvent(X, event)
+	m.SetEvent(Y, event)
+	m.SetEvent(Z, event)
+	err := cl.WaitForEvents([](*cl.Event){event})
+	if err != nil { fmt.Printf("WaitForEvents failed in addcubicanisotropy: %+v \n", err) }
+}
---
+++ opencl/dmibulk.go
@@ -0,0 +1,35 @@
+package opencl
+
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/mumax/3cl/opencl/cl"
+	"github.com/mumax/3cl/data"
+	"github.com/mumax/3cl/util"
+)
+
+// Add effective field due to bulk Dzyaloshinskii-Moriya interaction to Beff.
+func AddDMIBulk(Beff *data.Slice, m *data.Slice, Aex_red, D_red SymmLUT, regions *Bytes, mesh *data.Mesh) {
+	cellsize := mesh.CellSize()
+	N := Beff.Size()
+	util.Argument(m.Size() == N)
+	cfg := make3DConf(N)
+
+	event := k_adddmibulk_async(Beff.DevPtr(X), Beff.DevPtr(Y), Beff.DevPtr(Z),
+		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
+		unsafe.Pointer(Aex_red), unsafe.Pointer(D_red), regions.Ptr,
+		float32(cellsize[X]*1e9), float32(cellsize[Y]*1e9), float32(cellsize[Z]*1e9),
+		N[X], N[Y], N[Z], mesh.PBC_code(), cfg,
+		[](*cl.Event){Beff.GetEvent(X), Beff.GetEvent(Y), Beff.GetEvent(Z), 
+		m.GetEvent(X), m.GetEvent(Y), m.GetEvent(Z)})
+
+	Beff.SetEvent(X, event)
+	Beff.SetEvent(Y, event)
+	Beff.SetEvent(Z, event)
+	m.SetEvent(X, event)
+	m.SetEvent(Y, event)
+	m.SetEvent(Z, event)
+	err := cl.WaitForEvents([](*cl.Event){event})
+	if err != nil { fmt.Printf("WaitForEvents failed in adddmibulk: %+v \n", err) }
+}
