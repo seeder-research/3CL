87a7cfea90c3111802d08b2faf2d3b9905d20e8b
xfong <xuanyao.fong@gmail.com>
2017-04-17 11:13:25 +0800

    Fix package pointers for some test files

--- test/racetest.bash
+++ test/racetest.bash
@@ -4,7 +4,7 @@
 
 set -e
 
-go install -race github.com/mumax/3/cmd/mumax3
+go install -race github.com/mumax/3cl/cmd/mumax3
 
 google-chrome http://localhost:35367 &
 
@@ -12,5 +12,5 @@ for f in *.mx3; do
 	mumax3 $f 
 done
 
-go install github.com/mumax/3/cmd/mumax3 # re-build without race detector
+go install github.com/mumax/3cl/cmd/mumax3 # re-build without race detector
 
