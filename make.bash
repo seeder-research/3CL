1118073463b6a09fbdc6cf198360f0a0077f43df
xfong <xuanyao.fong@gmail.com>
2017-04-20 20:34:52 +0800

    Enable individual program install in make.bash

--- make.bash
+++ make.bash
@@ -1,11 +1,15 @@
 #! /bin/bash
+PROGRAMS=$1
+if [ -z "$PROGRAMS" ] || [ $PROGRAMS == "all" ]; then
+    PROGRAMS="..."
+fi
 
 ln -sf $(pwd)/pre-commit .git/hooks/pre-commit || echo ""
 ln -sf $(pwd)/post-commit .git/hooks/post-commit || echo ""
 
 (cd opencl/kernels_src/cl && ./make.bash)  || exit 1
 (cd opencl/kernels_src && ./make.bash)  || exit 1
-go install -v github.com/mumax/3cl/cmd/... || exit 1
+go install -v github.com/mumax/3cl/cmd/$PROGRAMS || exit 1
 #go vet github.com/mumax/3/... || echo ""
 #(cd test && mumax3 -vet *.mx3) || exit 1
 #(cd doc && mumax3 -vet *.mx3)  || exit 1
