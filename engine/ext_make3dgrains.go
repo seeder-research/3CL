cfccd772c54d3a4cd3386db5ce640914d4f8e767
Xuanyao Fong (Kelvin) <xuanyao.fong@gmail.com>
2018-03-27 00:11:11 +0800

    Fixed seed not being used in make3dgrains

--- engine/ext_make3dgrains.go
+++ engine/ext_make3dgrains.go
@@ -22,7 +22,6 @@ func Voronoi3d(grainsize float64, startRegion int, numRegions int, inputShape Sh
 type tesselation3d struct {
 	grainsize   float64
 	maxRegion   int
-	seed        int64
 	rnd         *rand.Rand
 	startRegion int
 	shape       Shape			//Shape of the tesselated region
@@ -44,8 +43,7 @@ func newTesselation3d(grainsize float64, nRegion int, seed int64, startRegion in
 	t := tesselation3d{grainsize,
 		nRegion,
 		//make(map[int3d][]center3d),
-		seed,
-		rand.New(rand.NewSource(0)),
+		rand.New(rand.NewSource(seed)),
 		startRegion,
 		inputShape,
 		make([]center3d, 0)}
@@ -140,8 +138,6 @@ func (t *tesselation3d) RegionOf(x, y, z float64) int {
 	} else {
 		return -1	//When the regions are rendered, any region < 0 will not be rastered.
 	}
-
-
 }
 
 // Generate normally distributed numbers; mean = lambda, variance = lambda. If generated number < 0, return 1.
